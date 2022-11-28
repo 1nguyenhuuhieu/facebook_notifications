a1914bbef9027b92b0849f7fd8c642697d6e29e8
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2022-11-28 17:53:22 +0700

    asd

--- load_cookie.py
+++ load_cookie.py
@@ -32,7 +32,7 @@ while True:
         news_btn.click()
 
         time.sleep(5)
-        share_btn = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[1]/div/div/div[4]/div/div/div[2]/div/div/div/div[2]/div/div[2]/div/div/div/div/div/div/div/div[1]/div/div[8]/div/div[4]/div/div/div[1]/div/div/div/div[3]/div/div[1]')
+        share_btn = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[1]/div/div/div[4]/div/div/div[2]/div/div/div/div[2]/div[2]/div[2]/div/div/div/div/div/div/div/div[1]/div/div[8]/div/div[4]/div/div/div[1]/div/div/div/div[3]/div')
         share_btn.click()
         time.sleep(1)
         copylink_btn = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div/div[3]/div/div/div/div[2]/div/div/div[1]/div[1]/div/div/div/div/div/div[1]/div/div[9]/div/div[1]')
