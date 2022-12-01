db8b151c1d2ec61d78c259a210a18688affac103
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2022-12-01 20:51:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -139,18 +139,16 @@ def notifications_listener():
         time.sleep(5)
         news_btn = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[2]/div/div[1]/div/div/div/div[1]/div[3]/div[2]/div/div/div[2]/div[1]')
         news_btn.click()
-        time.sleep(5)
+        time.sleep(15)
 
         post = driver.find_element(By.CSS_SELECTOR, "span[class='x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x3x7a5m x6prxxf xvq8zen xo1l8bm xzsf02u x1yc453h']")
         post = post.text.lower()
         time.sleep(2)
         if "see more" in post:
-            print("seemore")
             seemore_btn = driver.find_element(By.CSS_SELECTOR, "div[class='x1i10hfl xjbqb8w x6umtig x1b1mbwd xaqea5y xav7gou x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz xt0b8zv xzsf02u x1s688f']")
             
             seemore_btn.click()
-
-            print("seemore clickeed" )
+            
             time.sleep(3)
 
         time.sleep(5)
