2ce0beebe3b46db99abdc9bbdae577b32d5bbc2a
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2022-12-01 13:13:59 +0700

    dd

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ pwd_facebook = config["FACEBOOK"]["pwd"]
 
 
 sender_email = config["EMAIL"]["sender"]
-pwd = config["EMAIL"]["pwd"]
+pwd_email = config["EMAIL"]["pwd"]
 receiver_email = config["EMAIL"]["receiver"]
 
 keyword_filepath = config["KEYWORDS"]["file_path"]
@@ -97,6 +97,7 @@ def login_facebook(driver, home_url, cookies_filepath, pwd_facebook):
     count = 0
 
     while not is_logged:
+        driver.get(home_url)
         if count == 2:
             is_logged = True
 
@@ -107,89 +108,102 @@ def login_facebook(driver, home_url, cookies_filepath, pwd_facebook):
         
         except:
             count += 1
+            try:
+                account_link = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[1]/div/div/div/div[1]/div[4]/div[1]/div/div[1]/a[1]')
+                account_link.click()
 
-            account_link = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[1]/div/div/div/div[1]/div[4]/div[1]/div/div[1]/a[1]')
-            account_link.click()
+                time.sleep(3)
+                pwd_input = driver.find_element(By.XPATH, '/html/body/div[3]/div[2]/div/div/div/div/form/div[2]/div/input')
+                pwd_input.click()
+                pwd_input.send_keys(pwd_facebook)
+                time.sleep(10)
+                login_btn = driver.find_element(By.XPATH, '/html/body/div[3]/div[2]/div/div/div/div/form/div[4]/button')
+                login_btn.click()
 
-            time.sleep(3)
-            pwd_input = driver.find_element(By.XPATH, '/html/body/div[3]/div[2]/div/div/div/div/form/div[2]/div/input')
-            pwd_input.click()
-            pwd_input.send_keys(pwd_facebook)
-            time.sleep(5)
-            login_btn = driver.find_element(By.XPATH, '/html/body/div[3]/div[2]/div/div/div/div/form/div[4]/button')
-            login_btn.click()
-
-            time.sleep(5)
+                time.sleep(5)
+            except:
+                pass
 
     return None
 
 
-def test():
+def notifications_listener():
+    time.sleep(3)
+    found = False
+    keywords_found = []
+    try:
+        time.sleep(5)
+        unread_btn = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[2]/div/div[1]/div/div/div/div[1]/div[3]/div[1]/div[2]/div/span/span')
+        unread_btn.click()
 
+        time.sleep(3)
+        news_btn = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[2]/div/div[1]/div/div/div/div[1]/div[3]/div[2]/div/div/div[2]/div[1]')
+        news_btn.click()
+        time.sleep(5)
 
-    chrome_options = webdriver.ChromeOptions()
-    prefs = {"profile.default_content_setting_values.notifications" : 2}
-    chrome_options.add_experimental_option("prefs",prefs)
+        post = driver.find_element(By.CSS_SELECTOR, "span[class='x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x3x7a5m x6prxxf xvq8zen xo1l8bm xzsf02u x1yc453h']")
+        
 
+        if "See more" in post.text:
+            seemore_btn = driver.find_element(By.CSS_SELECTOR, "div[class='x1i10hfl xjbqb8w x6umtig x1b1mbwd xaqea5y xav7gou x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz xt0b8zv xzsf02u x1s688f']")
+            seemore_btn.click()
 
-    driver = webdriver.Chrome(executable_path="./chromedriver", chrome_options=chrome_options)
+        post_text = driver.find_element(By.CSS_SELECTOR, "span[class='x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x3x7a5m x6prxxf xvq8zen xo1l8bm xzsf02u x1yc453h']").text
+        post_text = post_text.lower()
 
+        for keyword in keywords:
+            if keyword in post_text:
+                found = True
+                keywords_found.append(keyword)
 
+        if found:
+            post_url = driver.current_url
+            list_str = post_url.split("/")
 
+            post = list_str[5]
+            list_post = post.split("=")
+            post_id = list_post[1].split("%")
+            post_id = post_id[0].split("&")
+            post_id = post_id[0]
 
-    count = 0
-    while True:
+            msg = f'Tìm thấy từ khóa "{keywords_found}" trong bài viết "{post_id}" tại group: {list_str[4]} \nNội dung bài viết:\n{post_text}'
 
-        found = False
-        keywords_found = []
+            print(msg)
 
-        try:
-            driver.get("https://www.facebook.com/notifications")
+            return msg
+        else:
+            return None
 
-            time.sleep(5)
-            unread_btn = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[2]/div/div[1]/div/div/div/div[1]/div[3]/div[1]/div[2]/div/span/span')
-            unread_btn.click()
-
-            time.sleep(3)
-            news_btn = driver.find_element(By.XPATH, '/html/body/div[1]/div[1]/div[1]/div/div[3]/div/div/div/div[1]/div[1]/div/div/div[2]/div/div[1]/div/div/div/div[1]/div[3]/div[2]/div/div/div[2]/div[1]')
-            news_btn.click()
-            count += 1
-
-            time.sleep(5)
-
-            post = driver.find_element(By.CSS_SELECTOR, "span[class='x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x3x7a5m x6prxxf xvq8zen xo1l8bm xzsf02u x1yc453h']")
-            
-
-            if "See more" in post.text:
-                seemore_btn = driver.find_element(By.CSS_SELECTOR, "div[class='x1i10hfl xjbqb8w x6umtig x1b1mbwd xaqea5y xav7gou x9f619 x1ypdohk xt0psk2 xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x16tdsg8 x1hl2dhg xggy1nq x1a2a7pz xt0b8zv xzsf02u x1s688f']")
-                seemore_btn.click()
-
-            post_text = driver.find_element(By.CSS_SELECTOR, "span[class='x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x3x7a5m x6prxxf xvq8zen xo1l8bm xzsf02u x1yc453h']").text
-            post_text = post_text.lower()
-
-            for keyword in keywords:
-                if keyword in post_text:
-                    found = True
-                    keywords_found.append(keyword)
-
-            if found:
-                post_url = driver.current_url
-                list_str = post_url.split("/")
-
-                print(f"Keyword: {keywords_found} in post {count}")
-                print(f"Post URL: {post_url} tại group {list_str[4]}")
-                print("*******")
-                print(post_text)
-                print("--------------------------")
-
-                msg = f'Tìm thấy từ khóa "{keywords_found}" trong bài viết {count} tại group: {list_str[4]} \nLink bài viết: {post_url} \n Nội dung bài viết:\n{post_text}'
-
-
-        except:
-            print("Try again")
+    except:
+        return None
 
 
 
+def goto_notifications_page():
+    try:
+        see_all_link = driver.find_element(By.CSS_SELECTOR, "a[class='x1i10hfl x1qjc9v5 xjqpnuy xa49m3k xqeqjp1 x2hbi6w x13fuv20 xu3j5b3 x1q0q8m5 x26u7qi x972fbf xcfux6l x1qhh985 xm0m39n x9f619 xdl72j9 x2lah0s xe8uvvx x2lwn1j xeuugli x16tdsg8 xggy1nq x1ja2u2z x1t137rt x1q0g3np x87ps6o x1lku1pv x1a2a7pz x1fey0fg x1ypdohk x92akz8 x1k74hu9 x76ihet xwmqs3e x112ta8 xxxdfa6 x1lcm9me x1yr5g0i xrt01vj x10y3i5r x1rg5ohu xdj266r x11i5rnm xat24cr x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 x3ajldb actionChildElement']")
+
+        see_all_link.click()
+    except:
+        pass
 
 driver = init_driver(driver_filepath)
 login_facebook(driver, home_url, cookies_filepath, pwd_facebook)
+
+
+driver.get(notifications_url)
+
+while True:
+    try:
+        msg = notifications_listener()
+        if msg:
+            send_notification_mail(sender_email, pwd_email, receiver_email, msg)
+        time.sleep(3)
+        goto_notifications_page()
+        time.sleep(5)
+
+    except:
+        pass
+
+
+
--- test.py
+++ test.py
@@ -1,16 +1,11 @@
-import configparser
+str = "https://www.facebook.com/groups/2436942816413232/?multi_permalinks=5720368821403932%2C5720364364737711%2C5720349264739221%2C5720343794739768%2C5720337841407030&notif_id=1669863541166489&notif_t=group_activity&ref=notif"
 
-import json
+list_str = str.split("/")
 
+post = list_str[5]
 
-config = configparser.ConfigParser()
-config.read('config.ini')
+list_post = post.split("=")
 
-keyword_filepath = config["KEYWORDS"]["file_path"]
-
-file = open(keyword_filepath, encoding='utf-8')
-cookies = json.load(file)
-file.close()
-t = [keyword for keyword in cookies.values()]
-
-print(t)
+post_id = list_post[1].split("%")
+post_id = post_id[0].split("&")
+print(post_id[0])
