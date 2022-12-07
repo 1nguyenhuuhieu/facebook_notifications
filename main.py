6c005ee6b2763b3340a0e4ae7a22407b2d1893dc
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2022-12-07 20:55:56 +0700

    asd

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from typing import Union
 import sqlite3
 import json
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
 
 from fastapi import FastAPI
 
 app = FastAPI()
+
+
+
 
 
 @app.get("/post-uncheck")
@@ -17,11 +24,17 @@ def read_root():
     jsonString = json.dumps(context)
     return jsonString
 
-@app.get("/post-checked")
+@app.get("/post-checked/")
 def post_checked():
     con = sqlite3.connect("fb.db")
     cur = con.cursor()
     res = cur.execute("SELECT * FROM post_checked")
+    context = res.fetchall()
+
+    c = "\n".join(list(context))
+
 
     con.close()
-    return res.fetchall()
+
+    return c
+    
---
+++ tracking.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route("/post-checked/")
+def hello_world(context = None):
+    con = sqlite3.connect("fb.db")
+    cur = con.cursor()
+    res = cur.execute("SELECT * FROM post_checked ORDER BY checked_time DESC LIMIT 100")
+    context = res.fetchall()
+    con.close()
+
+    return render_template('base.html', context=context)
