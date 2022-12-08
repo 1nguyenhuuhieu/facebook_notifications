90860a0f5536cea4b4ec11a486caac9b3ed280fc
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2022-12-08 19:40:01 +0700

    Update screenshot.py

--- screenshot.py
+++ screenshot.py
@@ -2,11 +2,5 @@ import pyscreenshot as ImageGrab
 
 # fullscreen
 im=ImageGrab.grab()
-im.show()
-
-# part of the screen
-im=ImageGrab.grab(bbox=(10,10,500,500))
-im.show()
-
 # to file
 ImageGrab.grab_to_file('im.png')
